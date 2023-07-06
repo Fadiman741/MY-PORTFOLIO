dbe9aa767002188b9e63640985b21b09a9be115c
Mafadi Lephaka <mafadilephaka97@gmail.com>
2023-07-06 21:03:36 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactmeComponent } from './contactme/contactme.component';
+import { BackgroundComponent } from './background/background.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+
+@NgModule({
+  declarations: [							
+    AppComponent,
+      HomePageComponent,
+      FooterComponent,
+      ContactmeComponent,
+      BackgroundComponent,
+      SkillsComponent,
+      ProjectsComponent
+   ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    // NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
